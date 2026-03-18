3921afca3a8af40986536eee3668a20ce4c01598
Dave Coleman <dave@dvclmn.com>
2026-03-18 10:59:42 +0100

    Incorporates changes by claude

--- Sources/CanvasKit/Modifiers/InteractionStateModifier.swift
+++ Sources/CanvasKit/Modifiers/InteractionStateModifier.swift
@@ -6,15 +6,16 @@
 //
 
 import BasePrimitives
-/// Bundles up the neccessary parts for callers to initialise state
-/// outside of CanvasView. This should be placed as high up the
-/// hierarchy as is needed for access.
 import SwiftUI
 
+/// Bundles up the necessary parts for callers to initialise state
+/// outside of CanvasView. This should be placed as high up the
+/// hierarchy as is needed for access.
 struct InteractionStateSetupModifier: ViewModifier {
   @Environment(\.modifierKeys) private var modifierKeys
   @State private var interactionState: CanvasInteractionState
   @Binding var toolHandler: ToolHandler
+
   init(
     state: CanvasInteractionState? = nil,
     toolHandler: Binding<ToolHandler>
@@ -24,42 +25,25 @@ struct InteractionStateSetupModifier: ViewModifier {
   }
 
   func body(content: Content) -> some View {
+
+    let snapshot = interactionState.snapshot
+    let policy = interactionState.activeTool.inputPolicy
+
     content
       .environment(interactionState)
-      .environment(\.zoomLevel, interactionState.snapshot.zoom)
-      .environment(\.panOffset, interactionState.snapshot.pan)
-      .environment(\.rotation, interactionState.snapshot.pan)
-      .environment(\.pointerLocation, snapshot.pointerLocation?.cgPoint)
-//      .environment(\.canvasOperation, operation)
-//      .environment(\.canvasInputPolicy, canvasPolicy)
-
+      .environment(\.canvasInputPolicy, policy)
+      .environment(\.zoomLevel, snapshot?.zoom ?? 1.0)
+      .environment(\.panOffset, snapshot?.pan ?? .zero)
+      .environment(\.rotation, snapshot?.rotation ?? .zero)
+      .environment(\.pointerLocation, snapshot?.pointerLocation?.cgPoint)
       .task(id: modifierKeys) { toolHandler.updateModifiers(modifierKeys) }
   }
 }
 
-extension InteractionStateSetupModifier {
-//  private var canvasPolicy: CanvasInputPolicy {
-//    .init(fromOperation: operation, tool: toolHandler.effectiveTool)
-//  }
-  /// The semantic operation currently being performed, computed from all input layers.
-//  private var operation: CanvasOperation {
-//    //  private func activeOperation(
-//    //    state: CanvasInteractionState,
-//    //    modifiers: Modifiers
-//    //  ) -> CanvasOperation {
-//    OperationResolver.resolve(
-//      state: interactionState,
-//      activeTool: toolHandler.effectiveTool,
-//      springLoadedTool: toolHandler.springLoadedTool,
-//      modifiers: modifierKeys
-//    )
-//  }
-
-}
 extension View {
   public func setUpInteractionState(
     _ state: CanvasInteractionState? = nil,
-    toolHandler: Binding<ToolHandler>,
+    toolHandler: Binding<ToolHandler>
   ) -> some View {
     self.modifier(
       InteractionStateSetupModifier(
