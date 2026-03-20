3de53a5b564702241659a04fca2b822fe0fa7c55
Dave Coleman <dave@dvclmn.com>
2026-03-20 10:13:37 +0100

    Thinking of making Global interaction also ...
    ... return a CanvasAdjustment?

--- Sources/CanvasKit/Views/CanvasToolbar.swift
+++ Sources/CanvasKit/Views/CanvasToolbar.swift
@@ -5,29 +5,29 @@
 //  Created by Dave Coleman on 19/3/2026.
 //
 
-import BasePrimitives
-import LayoutKit
-import SwiftUI
-
-struct CanvasToolbarView: ToolbarContent {
-  @Environment(CanvasInteractionState.self) private var interactionState
-  //  @Bindable var state: CanvasInteractionState
-  @Environment(\.zoomRange) private var zoomRange
-
-  var body: some ToolbarContent {
-
-    @Bindable var interactionState = interactionState
-    ToolbarItem {
-
-      if let zoomRange {
-        //          Slider(value: $store.transform.zoom.value, in: zoomRange)
-        QuickSlider("Zoom", value: $interactionState.transform.scale, range: zoomRange)
-          //          QuickSlider("Zoom", value: $store.transform.zoomState.value, range: zoomRange)
-
-          .frame(minWidth: 200)
-
-      }
-
-    }
-  }
-}
+//import BasePrimitives
+//import LayoutKit
+//import SwiftUI
+//
+//struct CanvasToolbarView: ToolbarContent {
+//  @Environment(CanvasInteractionState.self) private var interactionState
+//  //  @Bindable var state: CanvasInteractionState
+//  @Environment(\.zoomRange) private var zoomRange
+//
+//  var body: some ToolbarContent {
+//
+//    @Bindable var interactionState = interactionState
+//    ToolbarItem {
+//Text("HH")
+////      if let zoomRange {
+//        //          Slider(value: $store.transform.zoom.value, in: zoomRange)
+////        QuickSlider("Zoom", value: $interactionState.transform.scale, range: zoomRange)
+//          //          QuickSlider("Zoom", value: $store.transform.zoomState.value, range: zoomRange)
+//
+////          .frame(minWidth: 200)
+//
+////      }
+//
+//    }
+//  }
+//}
