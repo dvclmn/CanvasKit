5b78912a74c2fe3510a68225034a7e46a493a2c1
Dave Coleman <dave@dvclmn.com>
2026-03-28 10:54:17 +0100

    Dumps in some helper methods for defining...
    ... dependancies, figuring out deps between BaseHelpers, CanvasKit, GestureKit etc

--- Demo/CanvasDemo/ContentView.swift
+++ Demo/CanvasDemo/ContentView.swift
@@ -5,25 +5,25 @@
 //  Created by Dave Coleman on 9/1/2026.
 //
 
-import CanvasKit
-import SwiftUI
-import ToolKit
-
-struct ContentView: View {
-  let canvasSize: CGSize = CGSize(380, 300)
-  var body: some View {
-    CanvasView(canvasSize: canvasSize, showsInfoBar: true) {
-      Circle().fill(.brown.tertiary)
-        .padding()
-      Text("Hello")
-    }
-//    .background(.blue.quinary)
-  }
-}
-
-#if DEBUG
-#Preview {
-  ContentView()
-    .frame(minWidth: 400, minHeight: 500)
-}
-#endif
+//import CanvasKit
+//import SwiftUI
+//import ToolKit
+//
+//struct ContentView: View {
+//  let canvasSize: CGSize = CGSize(380, 300)
+//  var body: some View {
+//    CanvasView(canvasSize: canvasSize, showsInfoBar: true) {
+//      Circle().fill(.brown.tertiary)
+//        .padding()
+//      Text("Hello")
+//    }
+////    .background(.blue.quinary)
+//  }
+//}
+//
+//#if DEBUG
+//#Preview {
+//  ContentView()
+//    .frame(minWidth: 400, minHeight: 500)
+//}
+//#endif
--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.3
 
 import PackageDescription
 
@@ -17,10 +17,12 @@ let package = Package(
     .target(
       name: "CanvasKit",
       dependencies: [
-        .product(name: "SharedHelpers", package: "BaseHelpers"),
-        .product(name: "BaseMacros", package: "BaseHelpers"),
-        .product(name: "GestureKit", package: "BaseHelpers"),
-        .product(name: "BaseUI", package: "BaseHelpers"),
+        .module(.basePrimitives),
+        .module(.enumMacros),
+//        .product(name: "SharedHelpers", package: "BaseHelpers"),
+//        .product(name: "BaseMacros", package: "BaseHelpers"),
+//        .product(name: "GestureKit", package: "BaseHelpers"),
+//        .product(name: "BaseUI", package: "BaseHelpers"),
 
       ]
     ),
@@ -30,3 +32,58 @@ let package = Package(
     ),
   ]
 )
+
+extension Target.Dependency {
+//  static var demosCore: Self {
+//    .target(name: "DemosCore")
+//  }
+  static func module(_ dependency: BaseDependency) -> Self {
+    .product(
+      name: dependency.reference.0,
+      package: dependency.reference.1 ?? dependency.reference.0
+    )
+  }
+}
+extension String { static let baseHelpers = "BaseHelpers" }
+
+enum BaseDependency {
+  /// BaseHelpers
+  //  case gestureKit
+  case basePrimitives
+  case enumMacros
+  //  case sharedHelpers  // + Wrecktangle
+//  case baseUI
+  //  case layoutKit
+//  case graphicsKit
+  
+  /// Forwards-only
+//  case toolKit
+  //  case coreTools
+  
+  /// Third party
+//  case casePaths
+//  case sharing
+//  case metaCodable
+  
+  var reference: (String, String?) {
+    switch self {
+        
+        //      case .gestureKit: ("GestureKit", .baseHelpers)
+      case .basePrimitives: ("BasePrimitives", .baseHelpers)
+      case .enumMacros: ("BaseMacros", .baseHelpers)
+        //      case .sharedHelpers: ("SharedHelpers", .baseHelpers)
+//      case .baseUI: ("BaseUI", .baseHelpers)
+        //      case .layoutKit: ("LayoutKit", .baseHelpers)
+//      case .graphicsKit: ("GraphicsKit", .baseHelpers)
+        
+        //      case .coreTools: ("CoreTools", .baseHelpers)
+//      case .toolKit: ("ToolKit", .baseHelpers)
+        
+        /// Third party
+//      case .casePaths: ("CasePaths", "swift-case-paths")
+//      case .sharing: ("Sharing", "swift-sharing")
+//      case .metaCodable: ("MetaCodable", nil)
+        
+    }
+  }
+}
